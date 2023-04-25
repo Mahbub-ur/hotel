baff8ea2b2b742db5dbf4850d4a46261b4f849c2
Mahbub-ur <mahbuburrahman5658@gmail.com>
2023-04-25 12:34:20 +0600

    Primary done

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,73 @@
-const swiper = new Swiper('.mySwiper', {
-    // Optional parameters
-    direction: 'vertical',
+// Header Scroll
+let nav = document.querySelector(".navbar");
+window.onscroll = function () {
+    if (document.documentElement.scrollTop > 50) {
+        nav.classList.add("header-scrolled");
+    } else {
+        nav.classList.remove("header-scrolled");
+    }
+}
+
+// nav hide 
+let navBar = document.querySelectorAll(".nav-link");
+let navCollapse = document.querySelector(".navbar-collapse.collapse");
+navBar.forEach(function (a) {
+    a.addEventListener("click", function () {
+        navCollapse.classList.remove("show");
+    })
+})
+
+// Swiper Slider
+var swiper = new Swiper(".mySwiper", {
+    direction: "vertical",
     loop: true,
-  
-    // If we need pagination
     pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
+        el: ".swiper-pagination",
+        clickable: true,
     },
     autoplay: {
         delay: 3500,
     },
-  
-    // Navigation arrows
-    //   navigation: {
-    //     nextEl: '.swiper-button-next',
-    //     prevEl: '.swiper-button-prev',
-    // },
-  
-    // And if we need scrollbar
-    // scrollbar: {
-    //   el: '.swiper-scrollbar',
-    // },
+});
+
+
+
+// Owl Carousel
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        items:4,
+        loop:true,
+    });
   });
+
+
+// counter design
+$(".number").counterUp({time:3000});
+
+
+// Our Partner
+var swiper = new Swiper(".our-partner", {
+    slidesPerView: 5,
+    spaceBetween: 30,
+    loop: true,
+    autoplay: {
+        delay: 2000,
+    },
+    breakpoints: {
+        '991': {
+            slidesPerView: 5,
+            spaceBetween: 10,
+        },
+        '767': {
+            slidesPerView: 3,
+            spaceBetween: 10,
+        },
+        '320': {
+            slidesPerView: 2,
+            spaceBetween: 8,
+        },
+
+
+    },
+});
+
